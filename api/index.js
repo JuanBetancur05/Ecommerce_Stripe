eb330508ae4ecf1e587d47a8bd3f9d943a2423c1
JuanBetancur05 <juanesbetancur09@gmail.com>
2025-06-28 09:00:07 -0500

    vercel prueba5

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 import serverless from "serverless-http";
-import app from "../src/app.js"; // Importa la app sin arrancar el servidor
+import app from "../src/index.js"; // Importa la app sin arrancar el servidor
 
 export const handler = serverless(app);
